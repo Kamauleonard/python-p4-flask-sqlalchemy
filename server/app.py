c2d037d66ff718762969980f8aae1ef0d06b8806
Kamauleonard <leonardmwangi2012@gmail.com>
2024-01-24 15:12:03 +0300

    Initial Commit

--- server/app.py
+++ server/app.py
@@ -1,16 +1,17 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask, render_template
 
-from models import db
+from models import db, Owner, Pet
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-migrate = Migrate(app, db)
-
 db.init_app(app)
 
+@app.route('/')
+def index():
+    owners = Owner.query.all()
+    return render_template('index.html', owners=owners)
+
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
